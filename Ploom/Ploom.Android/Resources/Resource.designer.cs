3324a933098ffb4efc323c23b3f29978fcf6fddb
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2022-02-26 11:07:57 +0300

    update pages

--- Ploom/Ploom.Android/Resources/Resource.designer.cs
+++ Ploom/Ploom.Android/Resources/Resource.designer.cs
@@ -22000,16 +22000,19 @@ namespace Ploom.Droid
 			public const int Profil = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int search = 2131165332;
+			public const int qq = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int test_custom_background = 2131165333;
+			public const int search = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int tooltip_frame_dark = 2131165334;
+			public const int test_custom_background = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int tooltip_frame_light = 2131165335;
+			public const int tooltip_frame_dark = 2131165335;
+			
+			// aapt resource value: 0x7F070098
+			public const int tooltip_frame_light = 2131165336;
 			
 			static Drawable()
 			{
--- Ploom/Ploom/TabbedPages/SearchPage.xaml.cs
+++ Ploom/Ploom/TabbedPages/SearchPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Ploom.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,7 +14,46 @@ namespace Ploom.TabbedPages
     {
         public SearchPage()
         {
-            InitializeComponent();
+            InitializeComponent(); FirnitureService.All();
+            GoodsLstViw.ItemsSource = FirnitureService.lst;
+            //GoodsLstViw2.ItemsSource = FirnitureService.lst;
+        }
+
+        private void HomeBtn_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChairBtn_Clicked(object sender, EventArgs e)
+        {
+            CleanAllBtnBoxViews();
+            ChairBoxVw.IsVisible = true;
+        }
+
+        private void TableBtn_Clicked(object sender, EventArgs e)
+        {
+            CleanAllBtnBoxViews();
+            TableBoxVw.IsVisible = true;
+        }
+
+        private void ComodBtn_Clicked(object sender, EventArgs e)
+        {
+            CleanAllBtnBoxViews();
+            ComodBoxVw.IsVisible = true;
+        }
+
+        private void cupboard_Clicked(object sender, EventArgs e)
+        {
+            CleanAllBtnBoxViews();
+            CupboardBoxVw.IsVisible = true;
+        }
+
+        private void CleanAllBtnBoxViews()
+        {
+            ChairBoxVw.IsVisible = false;
+            TableBoxVw.IsVisible = false;
+            ComodBoxVw.IsVisible = false;
+            CupboardBoxVw.IsVisible = false;
         }
     }
 }
