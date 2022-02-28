32e3796e81faaa7d7d8abe40a6f32f79e725c820
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-02-28 12:47:35 +0300

    Add CheckoutPage

--- Ploom/Ploom/TabbedPages/CheckoutPage.xaml.cs
+++ Ploom/Ploom/TabbedPages/CheckoutPage.xaml.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ploom.Models;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Ploom.TabbedPages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class BusketPage : ContentPage
+    public partial class CheckoutPage : ContentPage
     {
-        public BusketPage()
+        public CheckoutPage()
         {
             InitializeComponent();
-            FirnitureService.All();
-            BusketLst.ItemsSource = FirnitureService.lst;
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new CheckoutPage());
+            await Navigation.PushAsync(new BusketPage());
         }
     }
 }
