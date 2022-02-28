95cb77862d890cd5dda35278922018eee9a725e8
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-02-28 10:54:23 +0300

    Add BusketPage

--- Ploom/Ploom/TabbedPages/BusketPage.xaml.cs
+++ Ploom/Ploom/TabbedPages/BusketPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Ploom.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,8 @@ namespace Ploom.TabbedPages
         public BusketPage()
         {
             InitializeComponent();
+            FirnitureService.All();
+            BusketLst.ItemsSource = FirnitureService.lst;
         }
     }
 }
--- Ploom/Ploom/TabbedPages/OfferFormPage.xaml.cs
+++ Ploom/Ploom/TabbedPages/OfferFormPage.xaml.cs
@@ -17,9 +17,9 @@ namespace Ploom.TabbedPages
             InitializeComponent();
         }
 
-        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
-
+            await Navigation.PushAsync(new OfferPage());
         }
     }
 }
