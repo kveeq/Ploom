c9e4325cd5cae30c7457fe7298e511eacb8a6afd
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-02-28 09:16:00 +0300

    Add OfferPage

---
+++ Ploom/Ploom/TabbedPages/OfferPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ploom.TabbedPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OfferPage : ContentPage
+    {
+        public OfferPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
--- Ploom/Ploom/TabbedPages/ProfilPage.xaml.cs
+++ Ploom/Ploom/TabbedPages/ProfilPage.xaml.cs
@@ -22,9 +22,9 @@ namespace Ploom.TabbedPages
 
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-
+            await Navigation.PushAsync(new OfferPage());
         }
 
         private async void Button_Clicked_1(object sender, EventArgs e)
